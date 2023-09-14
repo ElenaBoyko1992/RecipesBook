650441b1b995e808be1c3ae5cacc204548e081a3
ElenaBoyko1992 <Bionka@inbox.ru>
2023-09-14 12:57:23 +0300

    14.09.2023

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const axiosClient = axios.create({
 	baseURL: 'https://www.themealdb.com/api/json/v1/1/',
+	//withCredentials: true
 });
 
 export default axiosClient;
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 import axiosClient from "../axiosClient"
+
 export function searchMeals({ commit }, keyword) {
 	axiosClient.get(`search.php?s=${keyword}`)
 		.then(({ data }) => {
